e69d84d23f17307ac817bae2b9ea8404a4a32937
rpsilva <renan.silva@ext.hdi.com.br>
2020-08-30 11:48:24 -0300

    initial commit

---
+++ src/main/java/br/clientsapi/clientsapi/ClientsapiApplication.java
@@ -0,0 +1,13 @@
+package br.clientsapi.clientsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/clientsapi/clientsapi/ClientsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.clientsapi.clientsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
